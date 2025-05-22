a372ef807a37ec8160b8503bef21fcd36c3c8508
hanjw <jingwen.han@kpay-group.com>
2025-05-22 18:12:24 +0800

    feat：添加DataTable样例

--- lib/app/modules/list/views/list_view.dart
+++ lib/app/modules/list/views/list_view.dart
@@ -13,12 +13,41 @@ class ListView extends GetView<ListController> {
         title: const Text('ListView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ListView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: NewWidget(),
     );
   }
 }
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+        child: SingleChildScrollView(
+          child:DataTable(
+            columns: [
+              DataColumn(label: Text('姓名')),
+              DataColumn(label: Text('年龄')),
+              DataColumn(label: Text('性别')),
+              DataColumn(label: Text('简介')),
+            ],
+            rows: [
+              DataRow(
+                cells: [
+                  DataCell(Text('韩静文')),
+                  DataCell(Text('24')),
+                  DataCell(Text('女')),
+                  DataCell(Text('前端开发')),
+                ]
+              )
+            ],
+          )
+        ),
+    );
+  }
+}
+
+
